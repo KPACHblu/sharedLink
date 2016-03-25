f6f10a5462dd089a80cd20972f0cc3db143ca082
aubolut@gmail.com <Ut5esd729693>
2016-03-25 17:19:02 +0200

    Initial

---
+++ src/main/java/hello/ContentController.java
@@ -0,0 +1,57 @@
+package hello;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class ContentController {
+
+    @RequestMapping("/content")
+    public String index(@RequestParam(value = "url", defaultValue = "http://google.com") String url) {
+        return getContent(url);
+    }
+
+
+    private String getContent(String url) {
+// The underlying HTTP connection is still held by the response object
+// to allow the response content to be streamed directly from the network socket.
+// In order to ensure correct deallocation of system resources
+// the user MUST call CloseableHttpResponse#close() from a finally clause.
+// Please note that if response content is not fully consumed the underlying
+// connection cannot be safely re-used and will be shut down and discarded
+// by the connection manager.
+        String result = "";
+        CloseableHttpResponse response1 = null;
+        try {
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(url);
+            response1 = httpclient.execute(httpGet);
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            result = entity != null ? EntityUtils.toString(entity) : null;
+            EntityUtils.consume(entity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response1.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
+
+}
